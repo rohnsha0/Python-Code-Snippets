13dd6f9eba873d2ff425e244949af1db0634a091
Rohan Shaw <86848116+rohnsha0@users.noreply.github.com>
2021-07-03 00:57:33 +0530

    Create SurveyDataInsights.py
    We'd measured data from survey during Election!

---
+++ SurveyDataInsights.py
@@ -0,0 +1,10 @@
+majority_surveyed = "Party A"
+campaign_strength = "low"
+
+highly_active = majority_surveyed == "Party X"
+print("Finally Won, as per Survey?")
+print(highly_active)
+
+intensity = campaign_strength == "high"
+print("High intensity Campaigning?")
+print(intensity)
